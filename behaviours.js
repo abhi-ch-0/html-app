bd37d0be4fc57bbbb844735c6ce58c39b6c702db
“abhi” <abhichouhan@proton.me>
2024-06-23 00:53:36 +0530

    settings should appear at the bottom of the screen

--- behaviours.js
+++ behaviours.js
@@ -1,7 +1,7 @@
 function toggleSidebar() {
     var sidebar = document.getElementById('sidebar');
     var navIcon = document.getElementById('nav-icon');
-    if (sidebar.style.display === 'block') {
+    if (sidebar.style.display === 'flex' || sidebar.style.display === '') {
         sidebar.style.display = 'none';
         navIcon.innerHTML = `
             <rect x="10" y="20" width="80" height="10"></rect>
@@ -9,7 +9,7 @@ function toggleSidebar() {
             <rect x="10" y="70" width="80" height="10"></rect>
         `; // Hamburger icon
     } else {
-        sidebar.style.display = 'block';
+        sidebar.style.display = 'flex';
         navIcon.innerHTML = `
             <line x1="20" y1="20" x2="80" y2="80" stroke="white" stroke-width="10"/>
             <line x1="20" y1="80" x2="80" y2="20" stroke="white" stroke-width="10"/>
@@ -21,7 +21,7 @@ function handleResize() {
     var sidebar = document.getElementById('sidebar');
     var navIcon = document.getElementById('nav-icon');
     if (window.innerWidth > 768) {
-        sidebar.style.display = 'block';
+        sidebar.style.display = 'flex';
         navIcon.innerHTML = `
             <rect x="10" y="20" width="80" height="10"></rect>
             <rect x="10" y="45" width="80" height="10"></rect>
